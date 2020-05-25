45bcae70cf583baf82daf2253c2b80f16ca69d48
Krutarth Patel <kr739904@dal.ca>
2020-05-25 13:17:19 -0300

    added group info

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import './App.css';
 import Autocomplete from './Autocomplete';
 const App = () => {
   return (
-    <div className="App">      
+    <div className="App">
+
+    <div>
+        <p>
+          <b>CSCI5709 - Advanced Web Services</b>
+        </p>
+        <p>
+          <b>Group 19</b>
+          <br></br><br></br>
+          1. Darpan Patel (B00843607)
+          <br></br>
+          2. Deep Patel (B00845028)
+          <br></br>
+          3. Krutarth Patel (B00835794)
+          <br></br>
+          4. Niharika Prasad (B00835801)
+          <br></br>
+          5. Shwetha Subash (B00852743)
+          <br></br>
+          6. Yuganthi Krishnamurthy (B00839935)
+        </p>
+    </div>
+
+    <div>      
       <Autocomplete
         options={[
           'Web Development',
@@ -13,7 +36,8 @@ const App = () => {
           'Machine Learning',
           'Visual analytics',
         ]}
-      />
+      /> 
+      </div>
     </div>
   );
 };
