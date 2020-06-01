50df93b2a51f8499ec54666d3af139845f311671
ddpatel <dp827448@dal.ca>
2020-06-01 02:53:15 -0300

    fetch data using api

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Autocomplete.css';
 import Card from 'react-bootstrap/Card';
-
-let cardList;
+import { useTheme } from 'styled-components';
 
 export class Autocomplete extends Component {
   static propTypes = {
@@ -17,146 +16,65 @@ export class Autocomplete extends Component {
     this.state = {
       clicked: false,
       activeOptionIndex: 0,
-      filteredOptions: [],
+      movies: [],
       showOptions: false,
       userInput: ''
     };
   }
 
   onChange = (e) => {
-    const { options } = this.props;
     const userInput = e.target.value;
-    const filteredOptions = options.filter(
-      (option) => option.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-    );
-    this.setState({
-      activeOptionIndex: 0,
-      filteredOptions,
-      showOptions: true,
-      userInput
-    });
-    cardList="";
-  };
-
-  onClick = (e) => {
-    this.setState({
-      clicked: true,
-      activeOptionIndex: 0,
-      filteredOption: [],
-      showOptions: false,
-      userInput: e.currentTarget.innerText
-    });
-
-    cardList = {};
-    cardList = (
-      //Source: https://react-bootstrap.github.io/components/cards/
-
-      <div class="row " style={{ margin: '20px' }}>
-        <div class="col-md-3">
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title 1</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-      </Card.Text>
-          </Card.Body>
-        </Card>
-        </div>
-        <div class="col-md-3">
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title 2</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-      </Card.Text>
-          </Card.Body>
-        </Card>
-        </div>
-        <div class="col-md-3">
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title 3</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-      </Card.Text>
-          </Card.Body>
-        </Card>
-        </div>
-        <div class="col-md-3">
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title 4</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-      </Card.Text>
-          </Card.Body>
-        </Card>
-        </div>
-       
-      </div>
-     
-    );
-  };
-
-  onKeyDown = (e) => {
-    const { activeOptionIndex, filteredOptions } = this.state;
-    if (e.keyCode === 13) {     //select an item
-      this.setState({
-        activeOptionIndex: 0,
-        showSuggestions: false,
-        userInput: filteredOptions[activeOptionIndex]
-      });
-    } else if (e.keyCode === 38) {        //down arrow
-      if (activeOptionIndex === 0) {
-        return;
+    const url = `https://api-tutorial4.herokuapp.com/movies?title_like=${userInput}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          activeOptionIndex: 0,
+          movies: data,
+          showOptions: true,
+          userInput
+        })
       }
-      this.setState({ activeOptionIndex: activeOptionIndex - 1 });
-    } else if (e.keyCode === 40) {        //up arrow
-      if (activeOptionIndex - 1 === filteredOptions.length) {
-        return;
-      }
-      this.setState({ activeOptionIndex: activeOptionIndex + 1 });
-    }
+      )
+      .catch(err => console.log(err));
   };
-
   render() {
 
     const {
       onChange,
-      onKeyDown,
-      onClick,
-      state: { activeOptionIndex, filteredOptions, showOptions, userInput }
+      state: { activeOptionIndex, movies, showOptions, userInput }
     } = this;
 
-    let optionList;
-
-    if (showOptions && userInput) {
-      if (filteredOptions.length) {
-        optionList = (
-          <ul className="options">
-            {filteredOptions.map((optionName, index) => {
-              let className;
-              if (index === activeOptionIndex) {
-                className = 'option-active';
-              }
-              return (
-                <li className={className} key={optionName} onClick={onClick}>
-                  {optionName}
-                  {cardList}
-                </li>
-              );
-            })}
-          </ul>
-        );
-
-      } else {
-        optionList = (
-          <div className="no-options">
+     if (showOptions && userInput) {
+      if (movies.length) { return(
+        <div className="search">
+        <input type="text" className="search-box" placeholder="Enter a keyword!"
+          onChange={onChange}
+          value={userInput}
+        /><input type="submit" value="" className="search-btn" value="Search" />
+        <div className="row ml-4 mr-4 mt-4">
+            {this.state.movies.map((items, id) => { return(
+              <div className="col col-sm-3 mt-3" >
+                <Card style={{ width: '18rem', background: '#D4FACE', height: '10rem' }}>
+                  <Card.Body>
+                    <Card.Title>{items.title}</Card.Title>
+                    <Card.Text>{items.show_id}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+              )})}
+          </div>
+          </div>);
+      } else { return (
+        <div className="search">
+        <input type="text" className="search-box" placeholder="Enter a keyword!"
+          onChange={onChange}
+          value={userInput}
+        />
+        <input type="submit" value="" className="search-btn" value="Search" />
+          <div className="no-suggestions">
             <em>No Option!</em>
+          </div>
           </div>
         );
       }
@@ -165,14 +83,10 @@ export class Autocomplete extends Component {
       <div className="search">
         <input type="text" className="search-box" placeholder="Enter a keyword!"
           onChange={onChange}
-          onKeyDown={onKeyDown}
           value={userInput}
         />
-
         <input type="submit" value="" className="search-btn" value="Search" />
-        {optionList}
-        {this.state.clicked && <div>{cardList}</div>}
-      </div>
+       </div>
     );
   }
 }
