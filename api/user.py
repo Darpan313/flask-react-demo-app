9f5d4566a8c851ad8486b078b1cb000b1adb870d
ddpatel <dp827448@dal.ca>
2020-06-22 16:04:00 -0300

    Implement PUT request

--- api/user.py
+++ api/user.py
@@ -7,6 +7,10 @@ class User:
     def __init__(self,username,email):
         self.username=username
         self.email=email
+    
+    def setUsername(self, username):
+        self.username = username
+
 listOfUsers=[]
 user1=User("user1","user1@gmail.com")
 user2=User("user2","user2@gmail.com")
@@ -14,6 +18,12 @@ user2=User("user2","user2@gmail.com")
 listOfUsers.append(user1)
 listOfUsers.append(user2)
 
+def getIndex(email):
+    temp = None
+    for index, i in enumerate(listOfUsers):
+        if i.email == email:
+            temp = index
+    return temp
 
 @app.route('/getMethod',methods=['GET'])
 def getMethod():
@@ -27,6 +37,15 @@ def postMethod():
     listOfUsers.append(newUser)
     return "New User added!"
 
+@app.route('/putMethod',methods=['PUT'])
+def putMethod():
+    putData=request.json
+    index = getIndex(putData['email'])
+    if index:
+        listOfUsers[index].setUsername(putData['username'])
+        return "Username updated!"
+    else:
+        return "No User found with this email!"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
