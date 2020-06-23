e8fdd3da07df90a38d78c7ea454741891d330460
ddpatel <dp827448@dal.ca>
2020-06-23 13:02:26 -0300

    handle post method with existing Email

--- api/api.py
+++ api/api.py
@@ -3,20 +3,23 @@ from flask import Flask, request
 import json
 app = Flask(__name__, static_folder="../build", static_url_path='/')
 
+
 class User:
-    def __init__(self,username,email):
-        self.username=username
-        self.email=email
-    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
     def setUsername(self, username):
         self.username = username
 
-listOfUsers=[]
-user1=User("user1","user1@gmail.com")
-user2=User("user2","user2@gmail.com")
+
+listOfUsers = []
+user1 = User("user1", "user1@gmail.com")
+user2 = User("user2", "user2@gmail.com")
 
 listOfUsers.append(user1)
 listOfUsers.append(user2)
+
 
 def getIndex(email):
     temp = None
@@ -29,9 +32,11 @@ def getIndex(email):
         if temp is None:
             print("Invalid email address! Please enter a valid one.")
     except:
-        print("Something went wrong while fetching index of email address! Please try again.")
+        print(
+            "Something went wrong while fetching index of email address! Please try again.")
 
     return temp
+
 
 def getUserIndex(username):
     temp = None
@@ -40,7 +45,6 @@ def getUserIndex(username):
         for index, i in enumerate(listOfUsers):
             if i.username == username:
                 temp = index
-                print(temp)
 
         if temp is None:
             print("Invalid user name! Please enter a valid one.")
@@ -49,48 +53,52 @@ def getUserIndex(username):
 
     return temp
 
+
 @app.route('/')
 def index():
-	return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
+
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
 
 
-@app.route('/user/getMethod',methods=['GET'])
+@app.route('/user/getMethod', methods=['GET'])
 def getMethod():
-    jsonList=[]
+    jsonList = []
     try:
-        jsonList=json.dumps([ob.__dict__ for ob in listOfUsers])
+        jsonList = json.dumps([ob.__dict__ for ob in listOfUsers])
         if not jsonList:
             print("No users found!")
 
     except:
         print("Something went wrong while fetching users! Please try again.")
-    
+
     return jsonList
 
-@app.route('/user/postMethod',methods=['POST'])
+
+@app.route('/user/postMethod', methods=['POST'])
 def postMethod():
     try:
-        postData=request.json
-        newUser = User(postData['username'],postData['email'])
-        if newUser in listOfUsers:
-            print("User already exists")
-        else:    
+        postData = request.json
+        index = getIndex(postData['email'])
+        if index != None:
+            return "Email already exists"
+        else:
+            newUser = User(postData['username'], postData['email'])
             listOfUsers.append(newUser)
+            return "New User added!"
         if not listOfUsers:
             print("Error creating the user! Please try again.")
     except:
         print("Something went wrong while processing the user create request! Please try again.")
 
-    return "New User added!"
 
-@app.route('/user/putMethod',methods=['PUT'])
+@app.route('/user/putMethod', methods=['PUT'])
 def putMethod():
     try:
-        putData=request.json
+        putData = request.json
         index = getIndex(putData['email'])
         if index:
             listOfUsers[index].setUsername(putData['username'])
@@ -100,7 +108,8 @@ def putMethod():
     except:
         print("Something went wrong while processing the user update request! Please try again.")
 
-@app.route('/user/getUser',methods=['GET'])
+
+@app.route('/user/getUser', methods=['GET'])
 def getUser():
     try:
         postData = request.json
@@ -108,15 +117,14 @@ def getUser():
         print(index)
         if (index != None):
             print(listOfUsers[index])
-            user=json.dumps(listOfUsers[index].__dict__)
+            user = json.dumps(listOfUsers[index].__dict__)
             return user
         else:
             return "No User found with this username!"
     except:
-        print("Something went wrong while processing the getUser request! Please try again.")
+        print(
+            "Something went wrong while processing the getUser request! Please try again.")
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
-    
-
-        
+    app.run(host='0.0.0.0', port=5000)
